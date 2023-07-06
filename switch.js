d6541fa4dde37310e1efea24241a39bb4b2831a7
JvitorJ <joaovictorresende59@gmail.com>
2023-07-06 19:26:12 -0300

    aprendendo logica programacao com java script

---
+++ switch.js
@@ -0,0 +1,83 @@
+
+
+//Escreva um programa que receba o número de um mês (1 a 12) e exiba o nome do mês correspondente.
+
+let mes = "2";
+
+switch (mes) {
+    case "1":
+        console.log("Janeiro")
+        break;
+    case "2":
+        console.log("Fevereiro")
+        break;
+    case "3":
+        console.log("Março")
+        break;
+    case "4":
+        console.log("Abril")
+        break;
+    case "5":
+        console.log("Maio")
+        break;
+    case "6":
+        console.log("Junho")
+        break;
+    case "7":
+        console.log("Julho")
+        break;
+    case "8":
+        console.log("Agosto")
+        break;
+    case "9":
+        console.log("Setembro")
+        break;
+    case "10":
+        console.log("Outubro")
+        break;
+    case "11":
+        console.log("Novembro")
+        break;
+    case "12":
+        console.log("Dezembro")
+        break;
+    default:
+        console.log("Mês inválido")
+        break;
+}
+
+
+//Escreva um programa que receba um número de 1 a 7 e exiba o dia da semana correspondente.
+
+let diaDaSemana = "4";
+
+switch (diaDaSemana) {
+    case "1":
+        console.log("Domingo")
+        break;
+    case "2":
+        console.log("Segunda-feira")
+        break;
+    case "3":
+        console.log("Terça-feira")
+        break;
+    case "4":
+        console.log("Quarta-feira")
+        break;
+    case "5":
+        console.log("Quinta-feira")
+        break;
+    case "6":
+        console.log("Sexta-feira")
+        break;
+    case "7":
+        console.log("Sábado")
+        break;
+
+
+    default:
+        console.log("Dia da Semana não existente")
+        break;
+}
+
+//Escreva um programa que receba uma letra e verifique se é uma vogal ou uma consoante.
